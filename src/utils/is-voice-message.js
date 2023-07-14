4271810e2842c0025e41c41b43ed3bd4131ab36e
Kipicenko <heestons@gmail.com>
2023-07-14 21:56:43 +0300

    refactor is voice message function #2

--- src/utils/is-voice-message.js
+++ src/utils/is-voice-message.js
@@ -3,8 +3,7 @@
 export function isVoiceMessage(interaction) {
     let isAudio = null;
     if (!(interaction.attachments.size === 1)) return isAudio
-    for (const el of interaction.attachments) {
-        const [, currentAttachment] = el
+    for (const [, currentAttachment] of interaction.attachments) {
         isAudio = currentAttachment.contentType === 'audio/ogg' ? currentAttachment.url : null
     }
     return isAudio;
